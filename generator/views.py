699ee13ba80a7e4cb7677090263d45394de8f24b
Sherguioth <sherguioth@gmail.com>
2021-12-24 16:45:16 -0500

    feat: generator templates added

--- generator/views.py
+++ generator/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def about(request):
+    return render(request, 'generator/about.html')
+
+
+def home(request):
+    return render(request, 'generator/home.html')
+
+
+def password(request):
+    list('abcdefghijklmnopqrstuvwxyz')
+    return render(request, 'generator/password.html')
